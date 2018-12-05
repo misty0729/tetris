0b41dfbe39ac0186c9d3cbbb6138e0271d99ac7d
ella_yu <yuhuiran@126.com>
2018-12-05 09:29:51 +0800

    LI Rx MACRO: LI Rx Hi SLL Rx Rx 0 ADDIU Rx Lo
    CALL idt:
    LI R7 idt(hi)
    SLL R7 R7 0
    LI R7 idt(lo)
    JALR
    
    RETURN:
    JRRA
    
    B idt:
    B sign_ext(offset, 11)

--- converter.py
+++ converter.py
@@ -6,17 +6,17 @@ if len(sys.argv) < 2:
     sys.exit(0)
 
 # find asm
-ori_files = os.listdir(".")
+ori_files = os.listdir("./source")
 files = []
 for name in ori_files:
     if os.path.splitext(name)[-1] == '.asm':
-        print("find:" + name)
-        files.append(name)
+        print("find:" + "source/" + name)
+        files.append("source/" + name)
 
 # find main
 find_main = False
 for filename in files:
-    if filename == "main.asm":
+    if filename == "source/main.asm":
         find_main = True
 if not find_main:
     print("No main.asm. abort.")
@@ -37,11 +37,11 @@ for filename in files:
                 DEF_MACRO[line_idt[1]] = value
     f.close()
 print("find macros: " + str(len(DEF_MACRO)))
+print(DEF_MACRO)
 
 # set main as start
-files.remove("main.asm")
-files.insert(0, "main.asm")
-print(files)
+files.remove("source/main.asm")
+files.insert(0, "source/main.asm")
 
 line_list_1 = []
 for file in files:
@@ -60,61 +60,96 @@ for file in files:
             local_list_2.append(line)
     line_list_1.extend(local_list_2)
 
+# LI MACRO
 line_list_2 = []
 for line in line_list_1:
     line_idt = line.split()
     if line_idt[0] == "LI" and (line_idt[2] in DEF_MACRO):
-        print("find LI MACRO")
+        macro_value = DEF_MACRO[line_idt[2]]
+        print("MACRO: " + line_idt[2])
+        print(macro_value)
+        mv_hi = int(macro_value / 256)
+        mv_lo = macro_value % 256
+        line_list_2.append("LI " + line_idt[1] + " " + str(hex(mv_hi))[2:] + "\n")
+        print(line_list_2[-1])
+        line_list_2.append("SLL " + line_idt[1] + " " + line_idt[1] + " 0\n")
+        print(line_list_2[-1])
+        line_list_2.append("ADDIU " + line_idt[1] + " " + str(hex(mv_lo))[2:] + "\n")
+        print(line_list_2[-1])
     else:
         line_list_2.append(line)
 
+# CALL RET B
 line_list_3 = []
 for line in line_list_2:
-    if ':' in line:
-        idt = line[0:line.find(':') + 1]
-        next_line = line[line.find(':') + 1:]
-        line_list_3.append(idt)
-        line_list_3.append(next_line)
+    line_idt = line.split()
+    if line_idt[0] == "CALL":
+        line_list_3.append("LI R7 " + line_idt[1] + "\n")
+        line_list_3.append("SLL R7 R7 0\n")
+        line_list_3.append("ADDIU R7 " + line_idt[1] + "\n")
+        line_list_3.append("JALR R7\n")
+        print(line_list_3[-2])
+    elif line_idt[0] == "RETURN":
+        line_list_3.append("JRRA\n")
+        print(line_list_3[-1])
     else:
         line_list_3.append(line)
 
 line_list_4 = []
 for line in line_list_3:
-    if not len(line.split()) == 0:
+    if ':' in line:
+        idt = line[0:line.find(':') + 1]
+        next_line = line[line.find(':') + 1:]
+        line_list_4.append(idt)
+        line_list_4.append(next_line)
+    else:
         line_list_4.append(line)
 
-l_num = 0
+line_list_5 = []
 for line in line_list_4:
+    if not len(line.split()) == 0:
+        line_list_5.append(line)
+
+l_num = 0
+idt_list = {}
+for line in line_list_5:
     if ':' in line:
         idt = line[0:line.find(':')]
         idt_list[idt] = l_num
-        line_list_4[l_num] = "NOP\n"
+        line_list_5[l_num] = "NOP\n"
     l_num += 1
 
 print(idt_list)
 
 l_num = 0
-for line in line_list_4:
-    line_items = line.split()
-    for idt in idt_list:
-        if idt in line_items:
+for line in line_list_5:
+    line_idt = line.split()
+    for idt in line_idt:
+        if idt in idt_list:
             offset = idt_list[idt] - l_num - 1
-            if offset >= 0:
-                line_list_4[l_num] = line.replace(idt, str(hex(offset))[2:])
-            else:
-                offset += 256
-                if line_items[0] == "B":
-                    line_list_4[l_num] = line.replace(idt, "7" + str(hex(offset))[2:])
+            if line_idt[0] == "B":
+                if offset >= 0:
+                    line_list_5[l_num] = (line.replace(idt, str(hex(offset))[2:]))
                 else:
-                    line_list_4[l_num] = line.replace(idt, str(hex(offset))[2:])
-            print(line_list_4[l_num])
+                    offset += 2048
+                    line_list_5[l_num] = (line.replace(idt, str(hex(offset))[2:]))
+            elif line_idt[0] == "LI":
+                line_list_5[l_num] = line.replace(idt, str(hex(int(l_num / 256)))[2:])
+            elif line_idt[0] == "ADDIU":
+                line_list_5[l_num] = line.replace(idt, str(hex(l_num % 256))[2:])
+            else:
+                if offset < 0:
+                    offset += 256
+                line_list_5[l_num] = line.replace(idt, str(hex(offset))[2:])
+
+            # print(line_list_4[l_num])
     l_num += 1
 
 output = ""
-for line in line_list_4:
+for line in line_list_5:
     output += line
-print(output)
+# print(output)
 
-out_filename = sys.argv[2]
+out_filename = sys.argv[1]
 fout = open(out_filename, 'w')
 fout.write(output)
